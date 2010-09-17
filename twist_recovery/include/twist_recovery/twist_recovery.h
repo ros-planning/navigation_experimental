bc6a832ccf843f1cfb909056efcf56d4dac649c1
bhaskara <bhaskara@eb33c2ac-9c88-4c90-87e0-44a10359b0c3>
2010-09-17 00:00:14 +0000

    Adding twist_recovery

---
+++ twist_recovery/include/twist_recovery/twist_recovery.h
@@ -0,0 +1,105 @@
+/*
+ * Copyright (c) 2008, Willow Garage, Inc.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *     * Neither the name of the Willow Garage, Inc. nor the names of its
+ *       contributors may be used to endorse or promote products derived from
+ *       this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ */
+
+/**
+ * \file 
+ * 
+ * Recovery behavior based on executing a particular twist
+ *
+ * \author Bhaskara Marthi
+ */
+
+#ifndef TWIST_RECOVERY_TWIST_RECOVERY_H
+#define TWIST_RECOVERY_TWIST_RECOVERY_H
+
+#include <nav_core/recovery_behavior.h>
+#include <base_local_planner/costmap_model.h>
+#include <costmap_2d/costmap_2d_ros.h>
+#include <geometry_msgs/Pose2D.h>
+
+namespace twist_recovery
+{
+
+/// Recovery behavior that takes a given twist and tries to execute it for up to
+/// d seconds, or until reaching an obstacle.  
+class TwistRecovery : public nav_core::RecoveryBehavior
+{
+public:
+  
+  /// Doesn't do anything: initialize is where the actual work happens
+  TwistRecovery();
+
+  ~TwistRecovery();
+
+  /// Initialize the parameters of the behavior
+  void initialize (std::string n, tf::TransformListener* tf,
+                   costmap_2d::Costmap2DROS* global_costmap,
+                   costmap_2d::Costmap2DROS* local_costmap);
+
+  /// Run the behavior
+  void runBehavior();
+
+private:
+
+  geometry_msgs::Pose2D getCurrentLocalPose () const;
+  geometry_msgs::Twist scaleGivenAccelerationLimits (const geometry_msgs::Twist& twist, const double time_remaining) const;
+  double nonincreasingCostInterval (const geometry_msgs::Pose2D& current, const geometry_msgs::Twist& twist) const;
+  double normalizedPoseCost (const geometry_msgs::Pose2D& pose) const;
+  geometry_msgs::Twist transformTwist (const geometry_msgs::Pose2D& pose) const;
+
+  ros::NodeHandle nh_;
+  costmap_2d::Costmap2DROS* global_costmap_;
+  costmap_2d::Costmap2DROS* local_costmap_;
+  costmap_2d::Costmap2D costmap_; // Copy of local_costmap_, used by world model
+  std::string name_;
+  tf::TransformListener* tf_;
+  ros::Publisher pub_;
+  bool initialized_;
+
+  // Memory owned by this object
+  // Mutable because footprintCost is not declared const
+  mutable base_local_planner::CostmapModel* world_model_;
+
+  geometry_msgs::Twist base_frame_twist_;
+  
+  double duration_;
+  double linear_speed_limit_;
+  double angular_speed_limit_;
+  double linear_acceleration_limit_;
+  double angular_acceleration_limit_;
+  double controller_frequency_;
+  double simulation_inc_;
+  
+  
+};
+
+} // namespace twist_recovery
+
+#endif // include guard
---
+++ twist_recovery/src/twist_recovery.cpp
@@ -0,0 +1,214 @@
+/*
+ * Copyright (c) 2008, Willow Garage, Inc.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *     * Neither the name of the Willow Garage, Inc. nor the names of its
+ *       contributors may be used to endorse or promote products derived from
+ *       this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ */
+
+/**
+ * \file 
+ * 
+ * \author Bhaskara Marthi
+ * 
+ */
+
+#include <twist_recovery/twist_recovery.h>
+#include <pluginlib/class_list_macros.h>
+#include <tf/transform_datatypes.h>
+
+// register as a RecoveryBehavior plugin
+PLUGINLIB_DECLARE_CLASS(twist_recovery, TwistRecovery, twist_recovery::TwistRecovery,      
+                        nav_core::RecoveryBehavior)
+
+namespace gm=geometry_msgs;
+namespace cmap=costmap_2d;
+namespace blp=base_local_planner;
+using std::vector;
+using std::max;
+
+namespace twist_recovery
+{
+
+TwistRecovery::TwistRecovery () :
+  global_costmap_(NULL), local_costmap_(NULL), tf_(NULL), initialized_(false)
+{}
+
+TwistRecovery::~TwistRecovery ()
+{
+  delete world_model_;
+}
+
+void TwistRecovery::initialize (std::string name, tf::TransformListener* tf,
+                                cmap::Costmap2DROS* global_cmap, cmap::Costmap2DROS* local_cmap)
+{
+  ROS_ASSERT(!initialized_);
+  name_ = name;
+  tf_ = tf;
+  local_costmap_ = local_cmap;
+  global_costmap_ = global_cmap;
+  local_costmap_->getCostmapCopy(costmap_);
+  world_model_ = new blp::CostmapModel(costmap_);
+
+  pub_ = nh_.advertise<gm::Twist>("cmd_vel", 10);
+  ros::NodeHandle private_nh("~/" + name);
+
+  {
+  bool found=true;
+  found = found && private_nh.getParam("linear_x", base_frame_twist_.linear.x);
+  found = found && private_nh.getParam("linear_y", base_frame_twist_.linear.y);
+  found = found && private_nh.getParam("angular_z", base_frame_twist_.angular.z);
+  if (!found) {
+    ROS_FATAL_STREAM ("Didn't find twist parameters in " << private_nh.getNamespace());
+    ros::shutdown();
+  }
+  }
+
+  private_nh.param("duration", duration_, 1.0);
+  private_nh.param("linear_speed_limit", linear_speed_limit_, 0.3);
+  private_nh.param("angular_speed_limit", angular_speed_limit_, 1.0);
+  private_nh.param("linear_acceleration_limit", linear_acceleration_limit_, 4.0);
+  private_nh.param("angular_acceleration_limit", angular_acceleration_limit_, 3.2);
+  private_nh.param("controller_frequency", controller_frequency_, 20.0);
+  private_nh.param("simulation_inc", simulation_inc_, 1/controller_frequency_);
+
+  ROS_INFO_STREAM_NAMED ("top", "Initialized twist recovery with twist " <<
+                          base_frame_twist_ << " and duration " << duration_);
+  
+  initialized_ = true;
+}
+
+gm::Twist scaleTwist (const gm::Twist& twist, const double scale)
+{
+  gm::Twist t;
+  t.linear.x = twist.linear.x * scale;
+  t.linear.y = twist.linear.y * scale;
+  t.angular.z = twist.angular.z * scale;
+  return t;
+}
+
+gm::Pose2D forwardSimulate (const gm::Pose2D& p, const gm::Twist& twist, const double t=1.0)
+{
+  gm::Pose2D p2;
+  p2.x = p.x + twist.linear.x*t;
+  p2.y = p.y + twist.linear.y*t;
+  p2.theta = p.theta + twist.angular.z*t;
+  return p2;
+}
+
+/// Return the cost of a pose, modified so that -1 does not equal infinity; instead 1e9 does.
+double TwistRecovery::normalizedPoseCost (const gm::Pose2D& pose) const
+{
+  gm::Point p;
+  p.x = pose.x;
+  p.y = pose.y;
+  vector<gm::Point> oriented_footprint;
+  local_costmap_->getOrientedFootprint(pose.x, pose.y, pose.theta, oriented_footprint);
+  const double c = world_model_->footprintCost(p, oriented_footprint, local_costmap_->getInscribedRadius(),
+                                               local_costmap_->getCircumscribedRadius());
+  return c < 0 ? 1e9 : c;
+}
+
+
+/// Return the maximum d <= duration_ such that starting at the current pose, the cost is nonincreasing for
+/// d seconds if we follow twist
+/// It might also be good to have a threshold such that we're allowed to have lethal cost for at most
+/// the first k of those d seconds, but this is not done
+double TwistRecovery::nonincreasingCostInterval (const gm::Pose2D& current, const gm::Twist& twist) const
+{
+  double cost = normalizedPoseCost(current);
+  double t; // Will hold the first time that is invalid
+  for (t=simulation_inc_; t<=duration_; t+=simulation_inc_) {
+    const double next_cost = normalizedPoseCost(forwardSimulate(current, twist, t));
+    if (next_cost > cost) {
+      ROS_DEBUG_STREAM_NAMED ("cost", "Cost at " << t << " and pose " << forwardSimulate(current, twist, t)
+                              << " is " << next_cost << " which is greater than previous cost " << cost);
+      break;
+    }
+    cost = next_cost;
+  }
+  
+  return t-simulation_inc_;
+}
+
+double linearSpeed (const gm::Twist& twist)
+{
+  return sqrt(twist.linear.x*twist.linear.x + twist.linear.y*twist.linear.y);
+}
+
+double angularSpeed (const gm::Twist& twist)
+{
+  return fabs(twist.angular.z);
+}
+
+// Scale twist so we can stop in the given time, and so it's within the max velocity
+gm::Twist TwistRecovery::scaleGivenAccelerationLimits (const gm::Twist& twist, const double time_remaining) const
+{
+  const double linear_speed = linearSpeed(twist);
+  const double angular_speed = angularSpeed(twist);
+  const double linear_acc_scaling = linear_speed/(time_remaining*linear_acceleration_limit_);
+  const double angular_acc_scaling = angular_speed/(time_remaining*angular_acceleration_limit_);
+  const double acc_scaling = max(linear_acc_scaling, angular_acc_scaling);
+  const double linear_vel_scaling = linear_speed/linear_speed_limit_;
+  const double angular_vel_scaling = angular_speed/angular_speed_limit_;
+  const double vel_scaling = max(linear_vel_scaling, angular_vel_scaling);
+  return scaleTwist(twist, max(1.0, max(acc_scaling, vel_scaling)));
+}
+
+// Get pose in local costmap frame
+gm::Pose2D TwistRecovery::getCurrentLocalPose () const
+{
+  tf::Stamped<tf::Pose> p;
+  local_costmap_->getRobotPose(p);
+  gm::Pose2D pose;
+  pose.x = p.getOrigin().x();
+  pose.y = p.getOrigin().y();
+  pose.theta = tf::getYaw(p.getRotation());
+  return pose;
+}
+
+void TwistRecovery::runBehavior ()
+{
+  ROS_ASSERT (initialized_);
+
+  // Figure out how long we can safely run the behavior
+  const gm::Pose2D& current = getCurrentLocalPose();
+  local_costmap_->getCostmapCopy(costmap_); // This affects world_model_, which is used in the next step
+  
+  const double d = nonincreasingCostInterval(current, base_frame_twist_);
+  ros::Rate r(controller_frequency_);
+  ROS_INFO_NAMED ("top", "Applying (%.2f, %.2f, %.2f) for %.2f seconds", base_frame_twist_.linear.x,
+                   base_frame_twist_.linear.y, base_frame_twist_.angular.z, d);
+                   
+  // We'll now apply this twist open-loop for d seconds (scaled so we can guarantee stopping at the end)
+  for (double t=0; t<d; t+=1/controller_frequency_) {
+    pub_.publish(scaleGivenAccelerationLimits(base_frame_twist_, d-t));
+    r.sleep();
+  }    
+}
+
+
+} // namespace twist_recovery
+
